9b76c29be0b9fb47c91b0fa6c35951625b3cedc2
AndreaChiari <chiariandrea94@gmail.com>
2022-12-15 16:08:01 +0100

    update

--- pali.js
+++ pali.js
@@ -1,1 +1,48 @@
 console.log('JS OK');
+
+/* 
+1)chiedo all'utente di inserire una parola tramite un form
+2)creo una funzione o piu' che vadano a dettare delle condizioni affinche' la parola sia palindroma
+*/
+
+// prendo gli elementi dal DOM
+let input = document.getElementById("input").value;
+const button = document.getElementById("button");
+
+// Creo un event listener per memorizzare la parola scritta dall'utente
+button.addEventListener("click", function(){
+    input = document.getElementById("input").value;
+    console.log(input);
+
+// Validazione     
+if(!isNaN(input)){
+    alert("inserire solo parole!");
+}
+    
+// Creo una prima funzione per verificare il senso di una parola
+
+//DICHIARAZIONE
+function palindromefirst(word){
+ for(i=0; i < word.length; i++)
+ console.log(word);
+ return word;
+}
+
+ //INVOCAZIONE
+palindromefirst('Anna');
+
+// Creo una seconda funzione per verificare il senso di una parola
+
+//DICHIARAZIONE
+function reverseword(reverseword){
+    for(let i = reverseword.length -1;  i >= 0 ; i--)
+    result += reverseword[i];
+    console.log(reverseword[i]);
+    return reverseword;
+}
+
+//INVOCAZIONE 
+reverseword('Marco');
+
+    
+});
